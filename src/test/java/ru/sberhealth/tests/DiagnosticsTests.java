1999ef1af538251d40f7f2e07e94a5b6a250bfaf
Alex Ov <alexov@swisscows.email>
2023-07-14 02:39:32 +0300

    Add wait for city change by text.

--- src/test/java/ru/sberhealth/tests/DiagnosticsTests.java
+++ src/test/java/ru/sberhealth/tests/DiagnosticsTests.java
@@ -1,9 +1,11 @@
 package ru.sberhealth.tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 public class DiagnosticsTests extends TestBase {
@@ -18,6 +20,7 @@ public class DiagnosticsTests extends TestBase {
         });
         step ("Choose city", () -> {
             diagnosticsPage.chooseCity("Санкт-Петербург");
+            $(".about_list_short").shouldHave(Condition.text("в Санкт-Петербурге"));
         });
         step ("Choose diagnostics procedure", () -> {
             diagnosticsPage.chooseDiagnostics("4D УЗИ");
